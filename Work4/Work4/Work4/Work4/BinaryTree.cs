226cd3e2c2cdfaa60aa69f16713e8dc657d0559b
NachoGranados <ignaciogramar@gmail.com>
2020-08-03 00:46:43 -0600

    Binary Tree Code

---
+++ Work4/Work4/Work4/Work4/BinaryTree.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Work4
+{
+    class BinaryTree
+    {
+        public Node Root { get; set; }
+
+        public bool insert(int value)
+        {
+            Node before = null, after = this.Root;
+
+            while (after != null)
+            {
+                before = after;
+                if (value < after.Data)
+                    after = after.LeftNode;
+                else if (value > after.Data)
+                    after = after.RightNode;
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newNode = new Node();
+            newNode.Data = value;
+
+            if (this.Root == null)
+                this.Root = newNode;
+            else
+            {
+                if (value < before.Data)
+                    before.LeftNode = newNode;
+                else
+                    before.RightNode = newNode;
+            }
+
+            return true;
+        }
+
+        public void preOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.Write(parent.Data + " ");
+                preOrder(parent.LeftNode);
+                preOrder(parent.RightNode);
+            }
+        }
+
+        public void inOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                inOrder(parent.LeftNode);
+                Console.Write(parent.Data + " ");
+                inOrder(parent.RightNode);
+            }
+        }
+
+        public void postOrder(Node parent)
+        {
+            if (parent != null)
+            {
+                postOrder(parent.LeftNode);
+                postOrder(parent.RightNode);
+                Console.Write(parent.Data + " ");
+            }
+        }
+    }
+
+}
---
+++ Work4/Work4/Work4/Work4/Node.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Work4
+{
+    class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int Data { get; set; }
+    }
+
+}
